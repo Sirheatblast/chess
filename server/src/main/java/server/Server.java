745d6f0bdba0de024807a6449bba06ef8cc4f68d
Sirheatblast <tyjamcoombs@gmail.com>
2026-02-25 13:34:18 -0700

    Added endpoints for RegiserUser,LoginUser, and LogoutUser

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
+import handler.UserHandler;
 import io.javalin.*;
+import model.UserData;
 
 public class Server {
 
@@ -10,7 +12,23 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        javalin.post("/user", hdr -> {
+            UserData userData = hdr.bodyAsClass(UserData.class);
+            UserHandler usrHdlr = new UserHandler();
+            usrHdlr.RegisterUser(userData);
+        });
 
+        javalin.post("/session", hdr -> {
+            UserData userData = hdr.bodyAsClass(UserData.class);
+            UserHandler usrHdlr = new UserHandler();
+            usrHdlr.LoginUser(userData);
+        });
+
+        javalin.delete("/session", hdr -> {
+            UserData userData = hdr.bodyAsClass(UserData.class);
+            UserHandler usrHdlr = new UserHandler();
+            usrHdlr.LogOutUser(userData);
+        });
     }
 
     public int run(int desiredPort) {
