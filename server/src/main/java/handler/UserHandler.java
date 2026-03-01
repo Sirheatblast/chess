2325b4935496b4794a82749f0fc9c241c6815087
Sirheatblast <tyjamcoombs@gmail.com>
2026-02-28 20:30:32 -0700

    Implemented the final functions for the UserHandler class

--- server/src/main/java/handler/UserHandler.java
+++ server/src/main/java/handler/UserHandler.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import dataaccess.serverException.BadRequestException;
 import dataaccess.serverException.DataAccessException;
 import dataaccess.serverException.UserAlreadyExistsException;
+import dataaccess.serverException.UserUnauthorizedException;
 import io.javalin.http.Context;
+import model.AuthData;
 import model.UserData;
 import service.UserResult;
 import service.UserService;
@@ -12,8 +14,10 @@ import service.UserService;
 public class UserHandler {
     public void RegisterUser(Context cxt) {
         UserResult result;
+        Gson gson = new Gson();
+
         try{
-            UserData uData = cxt.bodyAsClass(UserData.class);
+            UserData uData = gson.fromJson(cxt.body(),UserData.class);
             UserService uService = new UserService();
             result = uService.RegisterUser(uData);
         }
@@ -27,16 +31,50 @@ public class UserHandler {
             result = new UserResult(500,e.getMessage(),"","");
         }
 
-        Gson gson = new Gson();
         String resultJson = gson.toJson(result);
         cxt.result(resultJson);
     }
 
     public void LoginUser(Context cxt) {
-        UserData userData = cxt.bodyAsClass(UserData.class);
+        UserResult result;
+        Gson gson = new Gson();
+
+        try{
+            UserData uData = gson.fromJson(cxt.body(),UserData.class);
+            UserService uService = new UserService();
+            result = uService.LoginUser(uData);
+        }
+        catch (UserUnauthorizedException e){
+            result = new UserResult(401,e.getMessage(),"","");
+        }
+        catch (BadRequestException e){
+            result = new UserResult(400,e.getMessage(),"","");
+        }
+        catch (Exception e){
+            result = new UserResult(500,e.getMessage(),"","");
+        }
+
+        String resultJson = gson.toJson(result);
+        cxt.result(resultJson);
     }
 
     public void LogOutUser(Context cxt) {
-        UserData userData = cxt.bodyAsClass(UserData.class);
+        UserResult result;
+        Gson gson = new Gson();
+
+        try{
+            AuthData aData = gson.fromJson(cxt.body(), AuthData.class);
+            UserService uService = new UserService();
+            result = uService.LogoutUser(aData);
+        }
+        catch (UserUnauthorizedException e){
+            result = new UserResult(401,e.getMessage(),"","");
+        }
+        catch (Exception e){
+            result = new UserResult(500,e.getMessage(),"","");
+        }
+
+        String resultJson = gson.toJson(result);
+        cxt.result(resultJson);
     }
 }
