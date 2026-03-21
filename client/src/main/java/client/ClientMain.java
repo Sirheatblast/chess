176a4287b0ca2f08973755458cab4d7c66f22e32
Sirheatblast <tyjamcoombs@gmail.com>
2026-03-21 13:13:23 -0600

    In ClientMain added bones for the logged out menu

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,64 @@
 package client;
 
-import chess.*;
+import model.UserData;
+
+import java.util.Scanner;
 
 public class ClientMain {
+    private static boolean isLoggedIn = false;
+    private static UserData uData;
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        while(true){
+            if(!isLoggedIn){
+                if(!preLoginUI()){
+                    break;
+                }
+            }
+            else{
+                if(loggedInUI()){
+                    break;
+                }
+            }
+        }
     }
+
+    private static boolean preLoginUI(){
+        System.out.print("Chess 240\n");
+        System.out.print("\n");
+        System.out.print("[Logged out]\n");
+
+        System.out.print("1: Create Account\n");
+        System.out.print("2: Login\n");
+        System.out.print("3: Help\n");
+        System.out.print("4: Quit\n");
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        switch (input){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                return false;
+            default:
+                System.out.print("Bad input\n");
+                break;
+        }
+
+        return true;
+    }
+
+    private static boolean registerUI(){
+
+        return true;
+    }
+
+    private static boolean loggedInUI(){
+        return true;
+    }
+
 }
