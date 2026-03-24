f6c62b665d1df6a1e03a85c6f6e66afa203cc9f9
Sirheatblast <tyjamcoombs@gmail.com>
2026-03-24 12:40:09 -0600

    Added three colors that I want to use for the border, white and black

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -1,5 +1,10 @@
 package ui;
 
 public class ChessBoard {
+    private static final int BoardSize = 8;
+    private static final char boarder =
 
+    public static void DrawBoard(chess.ChessBoard board){
+
+    }
 }
